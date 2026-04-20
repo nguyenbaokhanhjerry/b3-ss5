abd74dc2e90bd2dc85cd574a885a3c1e32d18ee6
nguyenbaokhanhjerry <khanhbaonguyen76@gmail.com>
2026-04-20 21:01:53 +0700

    Update b3 ss5.sql

--- b3 ss5.sql
+++ b3 ss5.sql
@@ -1,3 +1,11 @@
+-- 1. Phân tích bài toán (I/O)
+-- INPUT: min_trust_score, restaurant_id
+-- Kiểm tra đầu vào TRƯỚC KHI gọi SQL:
+-- IF min_trust_score < 0
+-- -> SET min_trust_score = 0 
+-- -> Log cảnh báo Admin nhập sai
+-- 	ELSE → tiếp tục bình thường
+-- 2 thực hiện
 SELECT driver_id, status, trust_score, distance_km
 FROM Drivers
 WHERE status = 'AVAILABLE'
